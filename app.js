02b66e359c720619acd33aa994cf8e235c7a4dbe
brent <brent.e.meyer@gmail.com>
2020-08-11 19:58:24 -0400

    splits out app set up from server

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const Mailer = require('./mail');
+const Cron = require('cron').CronJob;
+
+/*
+let job = new Cron(
+  // mintue, hour, day, month, day of week
+  // 0 21 * * * # 9pm
+  '0 21 * * *',
+  () => {
+    Mailer.sendDailyEmail();
+  },
+  null,
+  true,
+  'America/New_York',
+)
+*/
+
+app.disable('x-powered-by');
+
+app.use(morgan('---\n:date[clf]\n:method :url :status :response-time ms - :res[content-length]'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use('/api/v1', require('./routes'));
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,40 +1,7 @@
-"use strict";
-
 require('dotenv').config();
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
 
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-const Mailer = require('./mail');
-const Cron = require('cron').CronJob;
-
-/*
-let job = new Cron(
-  // mintue, hour, day, month, day of week
-  // 0 21 * * * # 9pm
-  '0 21 * * *',
-  () => {
-    Mailer.sendDailyEmail();
-  },
-  null,
-  true,
-  'America/New_York',
-)
-*/
-
-app.disable('x-powered-by');
-
-app.use(morgan('---\n:date[clf]\n:method :url :status :response-time ms - :res[content-length]'));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-app.use('/api/v1', require('./routes'));
+const port = process.env.PORT || 3001;
+const app = require('./app');
 
 app.listen(port, () => {
   console.log(`Listening on localhost:${port}`);
